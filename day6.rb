a653aad1704e2256870b3fa611c7a993add287fe
Ricardo Chavez <rchavez@devosinc.com>
2019-12-09 16:08:30 -0500

    Day 6 part 2

--- day6.rb
+++ day6.rb
@@ -1,5 +1,7 @@
 def parseOrbits(text)
   orbitMap = {}
+  santaObject = nil
+  myObject = nil
   text.each_line do |line|
     orbiting_around, object_in_orbit = line.chomp.split(")")
     if orbitMap[orbiting_around]
@@ -7,8 +9,14 @@ def parseOrbits(text)
     else
       orbitMap[orbiting_around] = [object_in_orbit]
     end
+    if object_in_orbit == "SAN"
+      santaObject = orbiting_around
+    end
+    if object_in_orbit == "YOU"
+      myObject = orbiting_around
+    end
   end
-  orbitMap
+  [orbitMap, santaObject, myObject]
 end
 
 def countOrbitsPerLevel(object, orbit_map, curr_level, level_map)
@@ -24,7 +32,19 @@ def countOrbitsPerLevel(object, orbit_map, curr_level, level_map)
   end
 end
 
-map = parseOrbits(ARGF.read)
+def tracePathToCOM(object, orbit_map)
+  parent = orbit_map.each_key.find { |k| k if orbit_map[k].include?(object) }
+  if parent
+    path = tracePathToCOM(parent, orbit_map)
+    return path << object
+  else
+    return []
+  end
+end
+
+map, santaObj, myObj = parseOrbits(ARGF.read)
+
+# Part 1
 lvl_map = {}
 countOrbitsPerLevel("COM", map, 0, lvl_map)
 totalCount = 0
@@ -32,4 +52,22 @@ lvl_map.each do |level, count|
   totalCount += (level * count)
 end
 
-puts totalCount
+puts "Part 1: #{totalCount}"
+
+# Part 2
+santaPath = tracePathToCOM(santaObj, map)
+myPath = tracePathToCOM(myObj, map)
+
+first_different_index = 0
+santaPath.zip(myPath).each_with_index do |objs, i|
+  if objs[0] != objs[1]
+    first_different_index = i
+    break
+  end
+end
+
+# puts first_different_index
+# puts santaPath[first_different_index..santaPath.length]
+# puts myPath[first_different_index..myPath.length]
+
+puts "Part 2: #{santaPath[first_different_index..santaPath.length].length + myPath[first_different_index..myPath.length].length}"
