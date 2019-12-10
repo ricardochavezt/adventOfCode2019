01059de91a29aa39b4503174d3dc38a7b664396a
Ricardo Chavez <rchavez@devosinc.com>
2019-12-10 12:02:59 -0500

    Day 7 part 1 refactor
    getting ready for part 2

--- day7.js
+++ day7.js
@@ -3,7 +3,43 @@ const intcodeComputer = require('./intcodeComputer');
 
 const rl = readline.createInterface({ input: process.stdin });
 
+function generatePhaseSettings(lowerPhaseSetting, upperPhaseSetting) {
+    let phaseSettings = [];
+    for (var a = lowerPhaseSetting; a < upperPhaseSetting; a++) {
+        for (var b = lowerPhaseSetting; b < upperPhaseSetting; b++) {
+            if (a == b) {
+                continue;
+            }
+            for (var c = lowerPhaseSetting; c < upperPhaseSetting; c++) {
+                if (c == a || c == b) {
+                    continue;
+                }
+                for (var d = lowerPhaseSetting; d < upperPhaseSetting; d++) {
+                    if (d == a || d == b || d == c) {
+                        continue;
+                    }
+                    for (var e = lowerPhaseSetting; e < upperPhaseSetting; e++) {
+                        if (e == a || e == b || e == c || e == d) {
+                            continue;
+                        }
+                        phaseSettings.push([a, b, c, d, e]);
+                    }
+                }
+            }
+        }
+    }
+    return phaseSettings;
+}
+
 let inputs = process.argv.slice(2).map(s => parseInt(s));
+let feedbackMode = inputs.shift();
+let phaseSettings;
+if (feedbackMode) {
+    phaseSettings = generatePhaseSettings(5, 10);
+}
+else {
+    phaseSettings = generatePhaseSettings(0, 5);
+}
 
 function runAmplifierControllerSoftware(code, phaseSetting, inputSignal) {
     let inputs = [phaseSetting, inputSignal];
@@ -15,37 +51,19 @@ function runAmplifierControllerSoftware(code, phaseSetting, inputSignal) {
 if (inputs.length > 0) {
     rl.on("line", line => {
         let maxThrusterSignal = 0;
-        for (var a = 0; a < 5; a++) {
-            for (var b = 0; b < 5; b++) {
-                if (a == b) {
-                    continue;
-                }
-                for (var c = 0; c < 5; c++) {
-                    if (c == a || c == b) {
-                        continue;
-                    }
-                    for (var d = 0; d < 5; d++) {
-                        if (d == a || d == b || d == c) {
-                            continue;
-                        }
-                        for (var e = 0; e < 5; e++) {
-                            if (e == a || e == b || e == c || e == d) {
-                                continue;
-                            }
-                            let outputA = runAmplifierControllerSoftware(line, a, inputs[0]);
-                            let outputB = runAmplifierControllerSoftware(line, b, outputA);
-                            let outputC = runAmplifierControllerSoftware(line, c, outputB);
-                            let outputD = runAmplifierControllerSoftware(line, d, outputC);
-                            let outputE = runAmplifierControllerSoftware(line, e, outputD);
-                            if (outputE > maxThrusterSignal) {
-                                maxThrusterSignal = outputE;
-                                console.log("Combination ", a, b, c, d, e, "gives max thruster signal", maxThrusterSignal);
-                            }
-                        }
-                    }
-                }
+        phaseSettings.forEach(phaseSetting => {
+            // console.log("Trying combination", phaseSetting);
+            let buffer = [inputs[0]];
+            let finalOutput = phaseSetting.reduce((acum, ampSetting) => {
+                let ampOutput = runAmplifierControllerSoftware(line, ampSetting, buffer.shift());
+                buffer.push(ampOutput);
+                return ampOutput;
+            }, 0);
+            if (finalOutput > maxThrusterSignal) {
+                maxThrusterSignal = finalOutput;
+                console.log("Combination ", phaseSetting, "gives max thruster signal", maxThrusterSignal);
             }
-        }
+        });
     }).on("close", () => console.log("Fin"));
 }
 else {
