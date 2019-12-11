bbbbd034df88030bfc30d6aafc412acc9728df4f
Ricardo Chavez <rchavez@devosinc.com>
2019-12-11 17:52:10 -0500

    Day 8 part 2

--- day8.go
+++ day8.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 )
 
 func main() {
@@ -16,11 +17,15 @@ func main() {
 		layerHeight, _ := strconv.Atoi(os.Args[2])
 		layerSize := layerWidth * layerHeight
 		minZeroCount := layerSize
+		image := make([]rune, layerSize)
+		for n := range image {
+			image[n] = '2'
+		}
 		for i := 0; i < len(line); i += layerSize {
 			zeroCount := 0
 			oneCount := 0
 			twoCount := 0
-			for _, r := range line[i : i+layerSize] {
+			for i2, r := range line[i : i+layerSize] {
 				switch r {
 				case '0':
 					zeroCount++
@@ -29,12 +34,19 @@ func main() {
 				case '2':
 					twoCount++
 				}
+				if image[i2] == '2' {
+					image[i2] = r
+				}
 			}
 			if zeroCount < minZeroCount {
 				result = oneCount * twoCount
 				minZeroCount = zeroCount
 			}
 		}
+		fmt.Println("Result:", result)
+		for l := 0; l < len(image); l += layerWidth {
+			s := string(image[l : l+layerWidth])
+			fmt.Println(strings.Replace(s, "0", " ", -1))
+		}
 	}
-	fmt.Println("Result:", result)
 }
