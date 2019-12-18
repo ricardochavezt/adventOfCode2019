384618f0862641392b6edec356a2a546ae44a6ba
Ricardo Chavez <rchavez@devosinc.com>
2019-12-18 17:23:34 -0500

    Day 12 part 2

--- day12.go
+++ day12.go
@@ -13,6 +13,27 @@ type Vector3D struct {
 	X, Y, Z int
 }
 
+// greatest common divisor
+func gcd(a, b int64) int64 {
+	for b > 0 {
+		temp := b
+		b = a % b
+		a = temp
+	}
+	return a
+}
+
+// least common multiple
+func lcm(a, b int64) int64 {
+	return a * (b / gcd(a, b))
+}
+
+// least common multiple of 3 numbers
+func lcm3(a, b, c int64) int64 {
+	result := lcm(a, b)
+	return lcm(result, c)
+}
+
 func updateVelocities(velocities, positions []Vector3D) {
 	for i, pos := range positions {
 		for j, otherPos := range positions {
@@ -64,17 +85,54 @@ func main() {
 		positions = append(positions, Vector3D{X: x, Y: y, Z: z})
 	}
 	velocities := make([]Vector3D, len(positions))
-	fmt.Println("positions:", positions, "velocities:", velocities)
-	for i := 1; i <= STEPS; i++ {
+	fmt.Println("initial state: positions:", positions, ", velocities:", velocities)
+	initialPositions := make([]Vector3D, len(positions))
+	copy(initialPositions, positions)
+	initialVelocities := make([]Vector3D, len(velocities))
+	copy(initialVelocities, velocities)
+	var cycleX, cycleY, cycleZ int64
+	for i := 1; true; i++ {
 		updateVelocities(velocities, positions)
 		updatePositions(positions, velocities)
+		if i == STEPS {
+			fmt.Println("positions:", positions, "velocities:", velocities)
+			totalEnergy := 0.0
+			for n := range positions {
+				potential := math.Abs(float64(positions[n].X)) + math.Abs(float64(positions[n].Y)) + math.Abs(float64(positions[n].Z))
+				kinetic := math.Abs(float64(velocities[n].X)) + math.Abs(float64(velocities[n].Y)) + math.Abs(float64(velocities[n].Z))
+				totalEnergy += (potential * kinetic)
+			}
+			fmt.Println("Total energy after", STEPS, "steps:", totalEnergy)
+		}
+		var equalStatesX, equalStatesY, equalStatesZ = true, true, true
+		for i2 := range positions {
+			if positions[i2].X != initialPositions[i2].X || velocities[i2].X != initialVelocities[i2].X {
+				equalStatesX = false
+			}
+			if positions[i2].Y != initialPositions[i2].Y || velocities[i2].Y != initialVelocities[i2].Y {
+				equalStatesY = false
+			}
+			if positions[i2].Z != initialPositions[i2].Z || velocities[i2].Z != initialVelocities[i2].Z {
+				equalStatesZ = false
+			}
+		}
+		if equalStatesX {
+			cycleX = int64(i)
+		}
+		if equalStatesY {
+			cycleY = int64(i)
+		}
+		if equalStatesZ {
+			cycleZ = int64(i)
+		}
+		if cycleX != 0 && cycleY != 0 && cycleZ != 0 {
+			fmt.Println("States repeat after N cycles: ", cycleX, "(X)", cycleY, "(Y)", cycleZ, "(Z)")
+			fmt.Println("Whole system should repeat after", lcm3(cycleX, cycleY, cycleZ), "cycles")
+			break
+		}
+		if i == 1000000000 {
+			fmt.Println("cycles not found after too many iterations")
+			break
+		}
 	}
-	fmt.Println("positions:", positions, "velocities:", velocities)
-	totalEnergy := 0.0
-	for n := range positions {
-		potential := math.Abs(float64(positions[n].X)) + math.Abs(float64(positions[n].Y)) + math.Abs(float64(positions[n].Z))
-		kinetic := math.Abs(float64(velocities[n].X)) + math.Abs(float64(velocities[n].Y)) + math.Abs(float64(velocities[n].Z))
-		totalEnergy += (potential * kinetic)
-	}
-	fmt.Println("Total energy after", STEPS, "steps:", totalEnergy)
 }
