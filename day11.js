371a659617ee2340438cbb1103cce0fb019e18b8
Ricardo Chavez <rchavez@devosinc.com>
2019-12-18 00:39:59 -0500

    Day 11 part 2

--- day11.js
+++ day11.js
@@ -1,13 +1,16 @@
 const readline = require('readline');
 const intcodeComputer = require('./intcodeComputer.js');
 
+let startingPanel = parseInt(process.argv[2]);
+
 const rl = readline.createInterface({ input: process.stdin });
 rl.on("line", line => {
     let outputs = [];
     let robotPosition = {x: 0, y:0};
     let robotDirection = "up";
     let paintedPanels = {};
-    let programState = intcodeComputer.compileAndRun(line, [0], outputs);
+    let programState = intcodeComputer.compileAndRun(line, [startingPanel], outputs);
+    let minX = robotPosition.x, maxX = robotPosition.x, minY = robotPosition.y, maxY = robotPosition.y;
     while (programState.exitOpcode != 99) {
         paintedPanels[`${robotPosition.x},${robotPosition.y}`] = outputs.shift();
         // primero giramos
@@ -45,6 +48,19 @@ rl.on("line", line => {
         let currentColor = paintedPanels[`${robotPosition.x},${robotPosition.y}`] || 0;
         // console.log("Color at", robotPosition, ":", currentColor);
         programState = intcodeComputer.resumeProgram(programState.programState, programState.instructionPointer, [currentColor], outputs, programState.relativeBase);
+        minX = Math.min(minX, robotPosition.x); maxX = Math.max(maxX, robotPosition.x);
+        minY = Math.min(minY, robotPosition.y); maxY = Math.max(maxY, robotPosition.y);
     }
     console.log(Object.keys(paintedPanels).length);
+    if (startingPanel == 1) { // Part 2
+        // console.log(paintedPanels);
+        for (var i = maxY; i >= minY; i--) {
+            let line = [];
+            for (var j = minX; j <= maxX; j++) {
+                let color = paintedPanels[`${j},${i}`] || 0;
+                line.push(color == 1 ? '⬜️' : '⬛️');
+            }
+            console.log(line.join(''));
+        }
+    }
 });
